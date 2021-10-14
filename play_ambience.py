6ef16c9ccb5cf8c6a2e03f5c343fead9a34f5e9e
addyett <g.aditya2048@gmail.com>
2021-10-14 19:22:36 +0530

    removing lint and better practices

--- play_ambience.py
+++ play_ambience.py
@@ -1,17 +1,21 @@
-from pomodorow import play
 import random
 import sys
+
+from pomodorow import play
 
 print(sys.argv)
 volume = float(sys.argv [1])
 print(volume)
 
-ambience = ['https://www.youtube.com/watch?v=9oc8Fa7tb8c', 'https://www.youtube.com/watch?v=q76bMs-NwRk', 'https://www.youtube.com/watch?v=LlKyGAGHc4c',
-            'https://www.youtube.com/watch?v=m5Mz9Tqs9CE']
+ambience = ['https://www.youtube.com/watch?v=9oc8Fa7tb8c',
+            'https://www.youtube.com/watch?v=q76bMs-NwRk',
+            'https://www.youtube.com/watch?v=LlKyGAGHc4c',
+            'https://www.youtube.com/watch?v=m5Mz9Tqs9CE',]
 
-shuffle = True
+SHUFFLE = True
 while True:
     print('started ambinece')
-    if shuffle: random.shuffle(ambience)
+    if SHUFFLE:
+        random.shuffle(ambience)
     for sound in ambience:
-        play(sound, ambience = True, volume = volume)
+        play(sound, volume=volume)
